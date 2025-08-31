105e0cd9db2a9178eda8bd3e2b4f0fb7fbb844ea
Naman Kumar <parnamnkh1@gmail.com>
2025-09-01 02:12:27 +0530

    Implement login functionality with form inputs and API integration; remove signup page.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,30 +3,36 @@ import {useRouter} from "next/navigation";
 import React from "react";
 import Link from "next/link";
 import {Axios} from "axios";
+import { userAgent } from "next/server";
+export default function Signup() {
+ const [user,setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+ });
+ const onSignup = async () =>{
 
-export default function Login() {
-    const [user,setUser] = React.useState({
-        email: "",
-        password: "",
-    });
-    const onLogin = async () => {
-        try {
-            const response = await Axios.post("http://localhost:3000/api/login",user);
-            console.log(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+ };
+ 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1>Login</h1>
+            <h1>Signup</h1>
             <hr/>
+            <label htmlFor="username">username</label>
+            <input
+            className="p-2 m-2 rounded-md border-2 border-gray-300"
+            id="username"
+            type="text"
+            placeholder="Enter username"
+            value={user.username}
+            onChange={(e) => setUser({...user,username: e.target.value})}
+            />
             <label htmlFor="email">email</label>
             <input
             className="p-2 m-2 rounded-md border-2 border-gray-300"
             id="email"
             type="email"
-            placeholder="Enter the email"
+            placeholder="Enter email"
             value={user.email}
             onChange={(e) => setUser({...user,email: e.target.value})}
             />
@@ -35,13 +41,13 @@ export default function Login() {
             className="p-2 m-2 rounded-md border-2 border-gray-300"
             id="password"
             type="password"
-            placeholder="Enter the password"
+            placeholder="Enter password"
             value={user.password}
             onChange={(e) => setUser({...user,password: e.target.value})}
             />
-            <button className="p-2 bg-blue-500 text-white rounded-md" onClick={onLogin}>Login</button>
+            <button className="p-2 bg-blue-500 text-white rounded-md" onClick={onSignup}>Signup</button>
             <hr/>
-            <Link href="/signup">visit Signup Page</Link>
+            <Link href="/login">visit Login Page</Link>
         </div>
     )
- }  
+}
